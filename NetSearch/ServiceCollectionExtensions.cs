fab1207727bc5d407278793f1877f51e8f76dfe2
Ricardo Peres <rjperes@hotmail.com>
2024-09-20 19:32:19 +0100

    Small improvements

--- NetSearch/ServiceCollectionExtensions.cs
+++ NetSearch/ServiceCollectionExtensions.cs
@@ -4,8 +4,20 @@ namespace NetSearch
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddGoogleSearch(this IServiceCollection services, Action<SearchOptions> options)
+        {
+            ArgumentNullException.ThrowIfNull(services, nameof(services));
+            ArgumentNullException.ThrowIfNull(options, nameof(options));
+
+            services.Configure(options);
+
+            return AddGoogleSearch(services);
+        }
+
         public static IServiceCollection AddGoogleSearch(this IServiceCollection services)
         {
+            ArgumentNullException.ThrowIfNull(services, nameof(services));
+
             services.AddHttpClient<ISearch, GoogleSearch>("Google", static client =>
             {
                 client.BaseAddress = new("https://google.com/search");
