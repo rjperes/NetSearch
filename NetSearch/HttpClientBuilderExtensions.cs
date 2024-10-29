38e5882ff2449f26eb27544fc42ce0f831db2f98
Ricardo Peres <rjperes@hotmail.com>
2024-10-29 12:39:55 +0000

    Fixed the RegisterKeyedService method

--- NetSearch/HttpClientBuilderExtensions.cs
+++ NetSearch/HttpClientBuilderExtensions.cs
@@ -7,32 +7,9 @@ namespace NetSearch
         public static IHttpClientBuilder RegisterKeyedService(this IHttpClientBuilder builder)
         {
             ArgumentNullException.ThrowIfNull(builder, nameof(builder));
+            ArgumentException.ThrowIfNullOrWhiteSpace(builder.Name, nameof(builder.Name));
 
-            if (string.IsNullOrWhiteSpace(builder.Name))
-            {
-                throw new ArgumentException("Registration name cannot be empty.");
-            }
-
-            var sd = builder.Services.Last(x => x.ServiceType == typeof(ISearch) && x.Lifetime == ServiceLifetime.Transient && x.ServiceKey == null && x.ImplementationFactory != null);
-
-            builder.Services.AddKeyedScoped(sd.ServiceType, builder.Name, (sp, name) => sd.ImplementationFactory!(sp));
-
-            //if (sd?.ImplementationFactory != null)
-            //{
-            //    builder.Services.AddKeyedScoped(sd.ServiceType, builder.Name, (sp, name) => sd.ImplementationFactory(sp));
-            //}
-            //else if (sd?.ImplementationInstance != null)
-            //{
-            //    builder.Services.AddKeyedScoped(sd.ServiceType, builder.Name, (sp, name) => sd.ImplementationInstance);
-            //}
-            //else if (sd?.ImplementationType != null)
-            //{
-            //    builder.Services.Add(new ServiceDescriptor(sd.ServiceType, builder.Name, sd.ImplementationType, ServiceLifetime.Transient));
-            //}
-            //else
-            //{
-            //    throw new InvalidOperationException("Invalid service registration.");
-            //}
+            builder.Services.AddKeyedTransient(builder.Name, (sp, key) => sp.GetRequiredService<IHttpClientFactory>().CreateClient((string)key);
 
             return builder;
         }
